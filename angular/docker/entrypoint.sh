b2fa8c5bd7247afda53773a0595e6d5bbae29125
jbuttler <jens_horst.buttler@smail.th-koeln.de>
2020-12-11 18:14:31 +0100

    removed unused files

--- angular/docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -d /app/node_modules ]; then
-    cd /app
-    npm install
-fi
-
-cd /app
-exec "${@}"
--- django/src/IOT_ETAGE/TestApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import request
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    print("Test 1")
-    print("Test 2")
-    return render(request, "html/htmlOne.html", {"title": "Test", "test": "YEAH!!!"})
