7d712c30fdd5b0246280ae304398b9b2983b8d49
jbuttler <jens_horst.buttler@smail.th-koeln.de>
2021-01-20 15:31:21 +0100

    debug script für kirill

--- CONTAINER/nodejs_worker/src/index.js
+++ CONTAINER/nodejs_worker/src/index.js
@@ -2,11 +2,14 @@ const mqtt = require('mqtt');
 require('dotenv').config();
 const axios = require('axios').default;
 
+let debug = false;
+
 let options = {
 	port: process.env.MQTT_PORT,
 	clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
 	username: process.env.MQTT_USER,
-	password: process.env.MQTT_PASS
+	password: process.env.MQTT_PASS,
+	rejectUnauthorized: false
 };
 let client = mqtt.connect(process.env.MQTT_HOST, options);
 
@@ -16,6 +19,20 @@ client.on('connect', () => {
 	client.subscribe(process.env.MQTT_SENSORDATA_TOPIC, (e) => {
 		if (e) console.log(`There was an error subscribing to ${process.env.MQTT_SENSORDATA_TOPIC}": ${e.message}`);
 	});
+
+	if (debug) {
+		setInterval(() => {
+			try {
+				obj = JSON.parse(`{"uid":1001,"sensorType":"TEMPERATURE","payload":${(Math.random() * (26.0 - 22.0) + 22.0).toFixed(1)}}`);
+				let response = axios.post('http://api:3002/sensordata', obj);
+				if (response.status >= 200 && response.status < 400) {
+					console.log(`Sensordata has been send successfully.`);
+				}
+			} catch (e2) {
+				console.error(e2);
+			}
+		}, 5000);
+	}
 });
 
 client.on('message', (topic, payload, packet) => {
